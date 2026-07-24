6db211c5d5068984a8e6efc116f9ef85150a6836
lottie <lottiekxiv@proton.me>
2026-07-23 21:13:46 -0500

    Automated git commit message at 23-07-2026 21:13:46

---
+++ flake.nix
@@ -0,0 +1,62 @@
+{
+  description = "A Nix-flake-based C/C++ development environment";
+
+  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0"; # stable Nixpkgs
+
+  outputs =
+    { self, ... }@inputs:
+
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            inherit system;
+            pkgs = import inputs.nixpkgs { inherit system; config.allowUnfree = true; };
+          }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs, system }:
+        {
+          default =
+            pkgs.mkShell.override
+              {
+                # Override stdenv in order to change compiler:
+                # stdenv = pkgs.clangStdenv;
+              }
+              {
+                packages =
+                  with pkgs;
+                  [
+                    clang-tools
+                    cmake
+                    codespell
+                    conan
+                    cppcheck
+                    doxygen
+                    gtest
+                    lcov
+                    vcpkg
+                    vcpkg-tool
+					gcc-arm-embedded
+					stm32flash
+					stm32cubemx
+					stm32loader
+                    self.formatter.${system}
+                  ]
+                  ++ lib.optionals (!stdenv.hostPlatform.isDarwin) [ gdb ];
+              };
+        }
+      );
+
+      formatter = forEachSupportedSystem ({ pkgs, ... }: pkgs.nixfmt);
+    };
+}
